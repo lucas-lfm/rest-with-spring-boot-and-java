284957c3339e58908259089c794029547850ef25
Lucas Ferreira Mendes <mendes.lucas.lfm@gmail.com>
2024-08-24 16:54:16 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/proflucas/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.proflucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
